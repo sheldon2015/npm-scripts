f71a794807dd2543960c63b2a7cbea3a528c2aca
sheldon2015 <823231684@qq.com>
2016-03-21 14:37:20 +0800

    初次提交

---
+++ dist/js/app.js
@@ -0,0 +1,1 @@
+var c,_=Function;with(o=document.getElementById("notice")){innerHTML+=innerHTML;onmouseover=_("c=1");onmouseout=_("c=0")}(F=_("if(#%18||!c)#++,#%=o.scrollHeight>>1;setTimeout(F,#%18?10:5000);".replace(/#/g,"o.scrollTop")))();
---
+++ src/js/index.js
@@ -0,0 +1,25 @@
+
+
+//公告轮播
+var c, _ = Function;
+
+with (o = document.getElementById("notice")) {
+
+    innerHTML += innerHTML;
+    onmouseover = _("c=1")
+    onmouseout = _("c=0");
+}
+/**
+ *
+ *   >> 位运算
+ * 
+ */
+
+(
+    F = _(
+
+        "if(#%18||!c)#++,#%=o.scrollHeight>>1;setTimeout(F,#%18?10:5000);".replace(/#/g, "o.scrollTop")
+
+    )
+
+)();
